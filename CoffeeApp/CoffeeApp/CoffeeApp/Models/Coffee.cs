0c67510f917baeb54bad0f179320d711c94ce377
Anteneh Bedilu <antenehdev@gmail.com>
2019-06-17 23:43:07 -0700

    Setting up Model, Services, & ViewModel

---
+++ CoffeeApp/CoffeeApp/CoffeeApp/Models/Coffee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoffeeApp.Models
+{
+    class Coffee
+    {
+        public int ID { get; set; }
+        public DateTime Date { get; set; }
+        public bool MadeAtHome { get; set; }
+        public string Location { get; set; }
+        public string OS { get; set; }
+
+    }
+}
---
+++ CoffeeApp/CoffeeApp/CoffeeApp/ViewModel/CoffeeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+using System.Linq;
+using System.Diagnostics;
+
+namespace CoffeeApp.ViewModel
+{
+    class CoffeeViewModel
+    {
+    }
+}
